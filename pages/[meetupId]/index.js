a8c874caff318d9fa6e72b8aa7e8a58749e077d1
yatin goyal <yatingoyal31@gmail.com>
2024-01-13 17:37:22 +0530

    added detailing page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import MeetupDetail from "../../components/meetups/Meetupdetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail image="https://th.bing.com/th/id/OIP.kBwvBKH-J2LZh0HImKaTAAHaE8?w=280&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7" title="A First Meetup" address="Some Street 5, Some City" description="The Meetup Description"/>
+  );
+}
+
+export default MeetupDetails;
+
