4dc7f19325d7eeabf7e485a8789b671121073699
yatin goyal <yatingoyal31@gmail.com>
2024-01-13 18:53:45 +0530

    added server side rendering

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2c63556514174ec"; }
+/******/ 		__webpack_require__.h = function() { return "203fbf4f8099e03b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
