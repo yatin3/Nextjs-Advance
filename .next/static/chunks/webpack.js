def7adf9270e5b444b4aa5855bcc2a79ead4ef5a
yatin goyal <yatingoyal31@gmail.com>
2024-01-13 18:38:27 +0530

    added static site generation prerendering

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e92b88b4f9a6861"; }
+/******/ 		__webpack_require__.h = function() { return "a2c63556514174ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,21 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
+function HomePage(props) {
+
   return (
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={props.meetups} />
   );
+}
+
+export async function getStaticProps(){
+  //fetch data from api
+  return {
+    props:{
+        meetups: DUMMY_MEETUPS
+    },
+    revalidate:10
+  };
 }
 
 export default HomePage;
