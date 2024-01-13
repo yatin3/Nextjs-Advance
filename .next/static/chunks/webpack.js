8cf67566b801cdcb6d36035f19d57bf95d6cead3
yatin goyal <yatingoyal31@gmail.com>
2024-01-13 16:14:32 +0530

    added appJs file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20f711952cef99f0"; }
+/******/ 		__webpack_require__.h = function() { return "3f39fdfd6854442d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import MeetupList from "../components/meetups/MeetupList";
-import Layout from "../components/layout/Layout"
 
 const DUMMY_MEETUPS = [
   {
@@ -22,9 +21,7 @@ const DUMMY_MEETUPS = [
 
 function HomePage() {
   return (
-    <Layout>
       <MeetupList meetups={DUMMY_MEETUPS} />
-    </Layout>
   );
 }
 
