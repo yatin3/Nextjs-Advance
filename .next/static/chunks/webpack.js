76a5e1a022656849636b25acd264ecd1ac0711f5
yatin goyal <yatingoyal31@gmail.com>
2024-01-14 15:01:07 +0530

    added post route to submit data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af9e129213f5e80d"; }
+/******/ 		__webpack_require__.h = function() { return "bcf650bb83cc15ce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
